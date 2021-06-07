da58cbadd88cd0413f4aea76b88d5908c5a5dbfe
pietro <pietrofacchini46@gmail.com>
2021-06-07 19:24:44 +0200

    New commit

--- src/app.cpp
+++ src/app.cpp
@@ -11,7 +11,7 @@ Display* displ;
 int screen;
 Window win;
 GC gc;
-unsigned long white, black, red, blue, background_color_dark, foreground_color_dark;
+unsigned long white, black, red, blue, current_forecolor, current_backgroundcolor, background_color_dark, foreground_color_dark, background_color_light, foreground_color_light;
 
 void init();
 void close();
@@ -35,9 +35,19 @@ int main()
 		char text[255];
 		KeySym key;
 		if(event.type==KeyPress && XLookupString(&event.xkey, text, 255, &key, 0)==1) {
-			if(text[0]=='q')
+			if(text[0]=='c')
 			{
-				close();
+				XClearWindow(displ, win);
+			}
+			if(text[0]=='w')
+			{
+				//XFreeGC(displ, gc);
+				XClearWindow(displ, win);
+			}
+			if(text[0]=='b')
+			{
+				//XFreeGC(displ, gc);
+				XClearWindow(displ, win);
 			}
 		}
 		if(event.type == ButtonPress)
@@ -57,16 +67,20 @@ int main()
 
 void init()
 {
-	dot.x=1920;
-	dot.y=1080;
+	dot.x=800;
+	dot.y=500;
 	displ=XOpenDisplay((char *)0);
 	screen=DefaultScreen(displ);
-	white=BlackPixel(displ, screen);
+	black=BlackPixel(displ, screen);
 	white=WhitePixel(displ, screen);
 	red=RGB(255, 0, 0);
 	blue=(0, 0, 255);
 	background_color_dark=RGB(40, 40, 40);
 	foreground_color_dark=RGB(210, 210, 210);
+	background_color_light=RGB(210, 210, 210);
+	foreground_color_light=RGB(40, 40, 40);
+	current_backgroundcolor=background_color_dark;
+	current_forecolor=foreground_color_dark;
 	win=XCreateSimpleWindow(displ, DefaultRootWindow(displ), 0, 0, dot.x, dot.y, 5, background_color_dark, background_color_dark);
 	XSetStandardProperties(displ, win, "plGE | Beta", "plGE | Beta", None, NULL, 0, NULL);
 	XSelectInput(displ, win, ExposureMask | ButtonPressMask | KeyPressMask);
